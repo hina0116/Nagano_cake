8bf3db304e2abb37f9774e4e816c591647632ab8
EC2 Default User <ec2-user@ip-172-31-43-230.ap-northeast-1.compute.internal>
2022-12-31 05:16:54 +0000

    [ADD]WBS_3.1.3

---
+++ app/controllers/admin/genres_controller.rb
@@ -0,0 +1,26 @@
+class Admin::GenresController < ApplicationController
+  def new
+    @genre = Genre.new
+  end
+
+  def create
+    @genre = Genre.new(genre_params)
+    @genre.save
+    redirect_to :index
+  end
+
+  def index
+    @genres = Genre.all
+  end
+
+  def edit
+    @genre = Genre.find(params[:id])
+  end
+
+  private
+
+  def genre_params
+    params.require(:genre).permit(:name)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
+  namespace :admin do
+    resources :genres, only: [:new, :create, :index, :edit]
+  end
 devise_for :customers,skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
